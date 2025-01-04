97686a844171316dbcecf424a208e2346027eeba
Adam Poper <adampoper@Adams-MacBook-Pro.local>
2025-01-04 01:11:05 -0500

    get, add, and delete entries

--- src/controllers/watch-list-controller.ts
+++ src/controllers/watch-list-controller.ts
@@ -2,7 +2,7 @@ import { Persistence } from '../persistence/persistence';
 import { Request, Response } from 'express';
 import dotenv from 'dotenv';
 import { WatchList, WATCH_LIST_TABLE, WatchListQueries } from '../entity/watch-list';
-import { WATCH_LIST_ENTRY_TABLE, WatchListEntryQueries } from '../entity/watch-list-entry';
+import { WATCH_LIST_ENTRY_TABLE, WatchListEntry, WatchListEntryModel, WatchListEntryQueries } from '../entity/watch-list-entry';
 
 dotenv.config();
 
@@ -44,7 +44,7 @@ const deleteWatchList = async (req: Request, res: Response) => {
         return;
     }
 
-    const entries = await Persistence.selectEntitiesByNamedQuery(WatchListEntryQueries.GET_WATCH_LIST_ENTRY_BY_WATCH_LIST_ID, [watchListId]);
+    const entries = await Persistence.selectEntitiesByNamedQuery(WatchListEntryQueries.GET_WATCH_LIST_ENTRIES_BY_WATCH_LIST_ID, [watchListId]);
     entries.forEach(async entry => await Persistence.deleteEntity(WATCH_LIST_ENTRY_TABLE, entry.id));
 
     Persistence.deleteEntity(WATCH_LIST_TABLE, watchListId)
@@ -52,4 +52,54 @@ const deleteWatchList = async (req: Request, res: Response) => {
         .catch(() => res.status(500).json({message: 'Error deleting watch list'}));
 }
 
-export default {addWatchList, getWatchLists, deleteWatchList};
+const addWatchListEntry = async (req: Request, res: Response) => {
+    const model = req.body as WatchListEntryModel;
+    if (!model.watchListId) {
+        res.status(400).json({message: 'No watch list id provided'});
+        return;
+    }
+
+    const existingEntry = await Persistence.selectEntityByNamedQuery(WatchListEntryQueries.GET_WATCH_LIST_ENTRY_BY_WATCH_LIST_ID_AND_SYMBOL, [model.watchListId, model.symbol]);
+    if (existingEntry) {
+        res.status(400).json({message: 'Watch list entry already exists'});
+        return;
+    }
+
+    const entry = {
+        watch_list_id: model.watchListId,
+        symbol: model.symbol
+    } as WatchListEntry;
+    console.log(entry);
+
+    Persistence.persistEntity(WATCH_LIST_ENTRY_TABLE, entry)
+        .then(() => res.status(200).json({message: 'Watch list entry added'}))
+        .catch(() => res.status(500).json({message: 'Error saving watch list entry'}));
+}
+
+const removeWatchListEntry = async (req: Request, res: Response) => {
+    const {watchListEntryId} = req.params;
+    console.log(watchListEntryId);
+
+    if (!watchListEntryId) {
+        res.status(400).json({message: 'No watch list entry id provided'});
+        return;
+    }
+
+    Persistence.deleteEntity(WATCH_LIST_ENTRY_TABLE, watchListEntryId)
+        .then(() => res.status(200).json({message: 'Watch list entry deleted'}))
+        .catch(() => res.status(500).json({message: 'Error deleting watch list entry'}));
+}
+
+const getWatchListEntries = async (req: Request, res: Response) => {
+    const {watchListId} = req.params;
+    if (!watchListId) {
+        res.status(400).json({message: 'No watch list id provided'});
+        return;
+    }
+
+    Persistence.selectEntitiesByNamedQuery(WatchListEntryQueries.GET_WATCH_LIST_ENTRIES_BY_WATCH_LIST_ID, [watchListId])
+        .then(entries => res.status(200).json(entries))
+        .catch(() => res.status(500).json({message: 'Error getting watch list entries'}));
+}
+
+export default {addWatchList, getWatchLists, getWatchListEntries, deleteWatchList, addWatchListEntry, removeWatchListEntry};
